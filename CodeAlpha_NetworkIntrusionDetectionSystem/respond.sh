9be135c1f346f2b21f9d98847fe285d69ab92b76
Ahndre Walters <waltersahndre@gmail.com>
2026-08-01 19:35:55 -0400

    Update respond.sh

--- CodeAlpha_NetworkIntrusionDetectionSystem/respond.sh
+++ CodeAlpha_NetworkIntrusionDetectionSystem/respond.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Auto-response script for Snort alerts
+# Auto-response script for Suricata alerts
 
-tail -f /var/log/snort/alert | while read line; do
+tail -f /var/log/suricata/fast.log | while read line; do
     if echo "$line" | grep -q "Port Scan Detected"; then
         IP=$(echo "$line" | grep -oE "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" | head -1)
         sudo iptables -A INPUT -s $IP -j DROP
